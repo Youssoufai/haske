be50c6a106136b4a3f56c3e3d01d9d1a28c7e5da
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-02-24 08:45:14 +0100

    hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Hero = () => {
     return (
         <>
-
+            <section className='flex'>
+                <div>
+                    <h2>Abdulrahman Haske</h2>
+                    <h1 className='text-4xl font-bold'>EMPOWERING TOMMOROW, UNITING TODAY</h1>
+                    <p>Welcome to the heart of democracy, Where Every Voice Resonates And Every Vote Shapes The Destiny Of Our Nation.</p>
+                </div>
+                <div>
+                    <img src="corporate.jpg" alt="" />
+                </div>
+            </section>
         </>
     )
 }
