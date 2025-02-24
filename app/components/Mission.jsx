15e83d623bc8c1efcc186b65b41f2f6b2196452b
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-02-24 14:29:39 +0100

    mission

--- app/components/Mission.jsx
+++ app/components/Mission.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Mission = () => {
     return (
         <>
-            <section className='h-screen'>
+            <section className='h-screen bg-slate-100 p-12'>
+                <div className='text-center space-y-3'>
+                    <h2 className='text-xs text-center text-green'>OUR MISSION & VISION</h2>
+                    <h1 className='text-3xl font-bold'>VISIONARY LEADERSHIP, UNITED PROGRESS</h1>
+                </div>
                 <div>
 
                 </div>
